f3b065615312087f8dd23f5408509ee44a22962f
GevinduM <gevindumadanayake@gmail.com>
2018-01-07 15:48:01 +0530

    Add files via upload

---
+++ login/ballot.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bit Secure Voting System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+     
+                <link rel="stylesheet" href="assets/css/w3.css">
+                <link rel="stylesheet" href="assets/css/modal.css">
+</head>
+<body>
+
+<br>
+</br>
+
+<div class="w3-container">
+ <center><h1><u>
+    Ballot Paper
+ </h1></u>
+ <p>Please Select Your Candidate</p></center>
+<br>
+</br>
+
+ </div>
+
+<center>  <div class="w3-red w3-hover-shadow w3-padding-64 w3-center" style="width:90%">
+  <br>
+</br>
+  <!-- Left-aligned media object -->
+  <div class="media">
+    <div class="media-left">
+      <img src="https://raw.githubusercontent.com/gevin93/e-voting-cdap/master/images/Maithripala-Sirisena.jpg" class="media-object" style="width:200px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading"></h4>
+      <h1>Maithripala Sirisena</h1>
+          <br>
+</br>
+               <center> <button type="button" class="btn btn-default btn-block btn-lg" data-toggle="modal" data-target="#myModal" style="width:150px">Vote</button></center>
+
+    
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Notice</h4>
+      </div>
+      <div class="modal-body">
+        <p>Your Vote Has Been Casted. Please Exit. Thank You</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+  </div>
+  </div>
+</div>
+  <hr>
+<div class="w3-green w3-hover-shadow w3-padding-64 w3-center" style="width:90%">
+  <div class="media">
+    <div class="media-left">
+      <img src="https://raw.githubusercontent.com/gevin93/e-voting-cdap/master/images/Ranil-Wickremesinghe.jpg" class="media-object" style="width:200px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading"></h4>
+    <h1>Ranil Wickramasinghe</h1>
+    <br>
+</br>
+                   <center> <button type="button" class="btn btn-default btn-block btn-lg" data-toggle="modal" data-target="#myModal" style="width:150px">Vote</button></center>
+    </div>
+
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Notice</h4>
+      </div>
+      <div class="modal-body">
+        <p>Your Vote Has Been Casted. Please Exit. Thank You</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+    </div>
+</div>
+</div>
+</div>
+<hr>
+<div class="w3-blue w3-hover-shadow w3-padding-64 w3-center" style="width:90%">
+  <div class="media">
+    <div class="media-left">
+      <img src="https://raw.githubusercontent.com/gevin93/e-voting-cdap/master/images/mahinda_rajapaksa.jpg" class="media-object" style="width:200px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading"></h4>
+      <h1>Mahinda Rajapaksha</h1>
+      <br>
+  </br>
+                     <center> <button type="button" class="btn btn-default btn-block btn-lg" data-toggle="modal" data-target="#myModal" style="width:150px">Vote</button></center>
+
+    </div>
+
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Notice</h4>
+      </div>
+      <div class="modal-body">
+        <p>Your Vote Has Been Casted. Please Exit. Thank You</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+  </div>    
+  </div>
+  
+
+
+
+  
+
+</div>
+
+</body>
+</html>
