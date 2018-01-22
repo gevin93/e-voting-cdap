7a463979d627bdb6d505a779e158a399f19f2cb3
gevin93 <gevindumadanayake@gmail.com>
2018-01-22 18:51:58 +0530

    Sub Pages of the Admin Dashboard - WIP

---
+++ admin_sub_page_emp/index.php
@@ -0,0 +1,92 @@
+<?php
+
+	require_once 'dbconfig.php';
+	
+	if(isset($_GET['delete_id']))
+	{
+		// select image from db to delete
+		$stmt_select = $DB_con->prepare('SELECT * FROM emp WHERE pin =:pin');
+		$stmt_select->execute(array(':pin'=>$_GET['delete_id']));
+		$imgRow=$stmt_select->fetch(PDO::FETCH_ASSOC);
+		unlink("user_images/".$imgRow['file']);
+		
+		// it will delete an actual record from db
+		$stmt_delete = $DB_con->prepare('DELETE FROM emp WHERE pin =:pin');
+		$stmt_delete->bindParam(':pin',$_GET['delete_id']);
+		$stmt_delete->execute();
+		
+		header("Location: index.php");
+	}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+<title>Bit Secure Voting System</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+
+<div class="container">
+
+	<div class="page-header">
+    	<h1 class="h2">Employee Profiles <a class="btn btn-primary" href="addnew.php"> <span class="glyphicon glyphicon-plus"></span> &nbsp; ADD New Employee </a></h1> 
+    </div>
+    
+<br />
+
+<div class="row">
+<?php
+	
+	$stmt = $DB_con->prepare('SELECT * FROM emp ORDER BY pin DESC');
+	$stmt->execute();
+	
+	if($stmt->rowCount() > 0)
+	{
+		while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+		{
+			extract($row);
+			?>
+			<div class="col-xs-3">
+				<p class="page-header"><?php echo $nic."&nbsp;/&nbsp;".$name; ?></p>
+				<img src="user_images/<?php echo $row['file']; ?>" class="img-rounded" width="250px" height="250px" />
+				<p class="page-header">
+				<span>
+				<a class="btn btn-info" href="editform.php?edit_id=<?php echo $row['pin']; ?>" title="click for edit" onclick="return confirm('sure to edit ?')"><span class="glyphicon glyphicon-edit"></span> Edit</a> 
+				<a class="btn btn-danger" href="?delete_id=<?php echo $row['pin']; ?>" title="click for delete" onclick="return confirm('sure to delete ?')"><span class="glyphicon glyphicon-remove-circle"></span> Delete</a>
+				</span>
+				</p>
+			</div>       
+			<?php
+		}
+	}
+	else
+	{
+		?>
+        <div class="col-xs-12">
+        	<div class="alert alert-warning">
+            	<span class="glyphicon glyphicon-info-sign"></span> &nbsp; No Data Found ...
+            </div>
+        </div>
+        <?php
+	}
+	
+?>
+</div>	
+
+
+
+
+</div>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
